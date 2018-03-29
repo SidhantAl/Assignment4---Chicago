68468cda8f32705707ea55e0d2f06520868702da
Mayshinlyan <hwai@u.rochester.edu>
2018-03-28 20:21:29 -0400

    updated the style of index

--- Chicago/index.php
+++ Chicago/index.php
@@ -1,15 +1,28 @@
-<?php include 'includes/html-top.php';?>
+<body>
+    <?php include 'includes/html-top.php';?>
 
-    <div class="container">
-
-        <!-- Navbar -->
-        <?php include 'includes/menu.php';?>
+    <!-- Navbar -->
+    <?php include 'includes/menu.php';?>
 
 
-			<header>
-        <h1>University of Rochester</h1>
-        <h2>Students with a Computer Science Double Major</h2>
-      </header>
+
+
+        <div id="hero">
+
+            <figure>
+                <img src="images/uor-logo.png" alt="Bei Lu">
+            </figure>
+
+            <h1>University of Rochester</h1>
+            <h3>Students with a Computer Science Double Major</h3>
+        </div>
+
+
+        <!-- slideshow -->
+        <script src="javascript/slideshow.js"></script>
+    
+        <div class="container">
+            
 
         <!--Code: Javascript Element goes here-->
         <div>
@@ -17,36 +30,36 @@
             <img class="mySlides" src="images/German.jpg">
             <img class="mySlides" src="images/Jovan.jpg">
         </div>
-
-        <!-- slideshow -->
-        <script src="javascript/slideshow.js"></script>
-
         <br><br><br>
-			<!-- Placeholder map -->
+        <!-- Placeholder map -->
 
-			<img src ="https://www.maps.com/media/catalog/product/cache/1/thumbnail/600x/17f82f742ffe127f42dca9de82fb58b1/u/s/usa-wall-map_1.jpg">
+        <img src="https://www.maps.com/media/catalog/product/cache/1/thumbnail/600x/17f82f742ffe127f42dca9de82fb58b1/u/s/usa-wall-map_1.jpg">
 
-      <p>There are many amazing students at the University of Rochester who are planning to graduate with two Majors. Here you can find profiles on three such students, each majoring in Computer Science and one other subject.</p>
+        <p>There are many amazing students at the University of Rochester who are planning to graduate with two Majors. Here you can find profiles on three such students, each majoring in Computer Science and one other subject.</p>
 
-      <div class="form">
-        <form action="welcome.php" method="post">
-        Name:<br>
-        <input type="text" name="name" value="Name"><br>
-        Major 1:<br>
-        <input type="text" name="major1" value="Major1"><br>
-        Major 2:<br>
-        <input type="text" name="major2" value="Major2"><br>
-        Class year:<br>
-        <input type="text" name="class" value="Class"><br>
-        Gender:<br>
-        <input type="radio" name="gender" value="Male"> Male<br>
-        <input type="radio" name="gender" value="Female"> Female<br>
-        <input type="radio" name="gender" value="Other"> Other<br><br>
-        <input type="submit"  onclick="alert('Thank you!')" value="Submit">
-      </div><!--form class-->
+        <div class="form">
+            <form action="welcome.php" method="post">
+                Name:<br>
+                <input type="text" name="name" value="Name"><br> Major 1:<br>
+                <input type="text" name="major1" value="Major1"><br> Major 2:<br>
+                <input type="text" name="major2" value="Major2"><br> Class year:<br>
+                <input type="text" name="class" value="Class"><br> Gender:
+                <br>
+                <input type="radio" name="gender" value="Male"> Male<br>
+                <input type="radio" name="gender" value="Female"> Female<br>
+                <input type="radio" name="gender" value="Other"> Other<br><br>
+                <input type="submit" onclick="alert('Thank you!')" value="Submit">
+                
+            </form>
+        </div>
+        <!--form class-->
 
     </div>
 
-    <script src="javascript/menu-highlighter.js"></script>
-  </body>
+    <footer>
+        &copy; 2018 Team Chicago
+    </footer>
+        
+</body>
+
 </html>
