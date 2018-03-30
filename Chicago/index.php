d9e8b47e07d535b0fe0a3d98e4c4bdda22161634
Louis <lfusilli@u.rochester.edu>
2018-03-30 18:38:46 -0400

    added alt tags

--- Chicago/index.php
+++ Chicago/index.php
@@ -26,14 +26,14 @@
 
         <!--Code: Javascript Element goes here-->
         <div>
-            <img class="mySlides" src="images/Andrew.jpg">
-            <img class="mySlides" src="images/German.jpg">
-            <img class="mySlides" src="images/Jovan.jpg">
+            <img class="mySlides" alt="Andrew" src="images/Andrew.jpg">
+            <img class="mySlides" alt="German" src="images/German.jpg">
+            <img class="mySlides" alt="Jovan" src="images/Jovan.jpg">
         </div>
         <br><br><br>
         <!-- Placeholder map -->
 
-        <img src="https://www.maps.com/media/catalog/product/cache/1/thumbnail/600x/17f82f742ffe127f42dca9de82fb58b1/u/s/usa-wall-map_1.jpg">
+        <img alt="maps" src="https://www.maps.com/media/catalog/product/cache/1/thumbnail/600x/17f82f742ffe127f42dca9de82fb58b1/u/s/usa-wall-map_1.jpg">
 
         <p>There are many amazing students at the University of Rochester who are planning to graduate with two Majors. Here you can find profiles on three such students, each majoring in Computer Science and one other subject.</p>
 
